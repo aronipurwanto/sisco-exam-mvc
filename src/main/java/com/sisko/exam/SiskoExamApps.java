47bdf809a94d2ec5b54730c9141786924d674012
Roni Purwanto <roni.purwanto@amartha.com>
2025-11-20 17:06:55 +0700

    First Commit

---
+++ src/main/java/com/sisko/exam/SiskoExamApps.java
@@ -0,0 +1,13 @@
+package com.sisko.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiskoExamApps {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiskoExamApps.class, args);
+    }
+
+}
---
+++ src/test/java/com/sisko/exam/SiskoExamAppsTests.java
@@ -0,0 +1,13 @@
+package com.sisko.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiskoExamAppsTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
