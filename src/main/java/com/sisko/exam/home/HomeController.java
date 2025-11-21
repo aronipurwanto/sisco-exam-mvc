a8a29e6fad306a1b6abba336691c2b245330362e
Roni Purwanto <roni.purwanto@amartha.com>
2025-11-21 08:53:35 +0700

    feat: fixing template

--- src/main/java/com/sisko/exam/home/HomeController.java
+++ src/main/java/com/sisko/exam/home/HomeController.java
@@ -2,18 +2,16 @@ package com.sisko.exam.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("pages/home/index");
+    public String index() {
+        return "pages/home/index";
     }
 
     @GetMapping("/dashboard")
-    public ModelAndView dashboard(){
-        return new ModelAndView("pages/home/dashboard");
+    public String dashboard(){
+        return "pages/home/dashboard";
     }
 }
